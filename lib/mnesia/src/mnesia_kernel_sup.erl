9ccdf6b94cb799167f18d822538e6f0e9367756d
Rick Reed <rr@whatsapp.com>
2012-05-02 08:48:52 -0700

    Use "debug" shutdown (24 hour) timeouts in attempt to avoid shutdown failures.

--- lib/mnesia/src/mnesia_kernel_sup.erl
+++ lib/mnesia/src/mnesia_kernel_sup.erl
@@ -55,7 +55,9 @@ worker_spec(Name, KillAfter, Modules) ->
 supervisor_spec(Name) ->
     {Name, {Name, start, []}, permanent, infinity, supervisor,
      [Name, supervisor]}.
-    
+
+-define(debug_shutdown, 1).
+
 -ifdef(debug_shutdown).
 supervisor_timeout(_KillAfter) -> timer:hours(24).
 -else.
