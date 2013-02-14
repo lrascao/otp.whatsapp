49e974f7b18db198f8eabcf004998805d5b3541e
Rick Reed <rr@whatsapp.com>
2013-02-14 10:39:59 -0800

    Add support for multiple timer wheel instances (default to one per scheduler).

--- erts/emulator/beam/time.c
+++ erts/emulator/beam/time.c
@@ -83,8 +83,10 @@
 #define ASSERT_NO_LOCKED_LOCKS
 #endif
 
-static erts_smp_mtx_t tiw_lock;
-
+typedef struct ErlTimerHead_ {
+    ErlTimer* head;
+    ErlTimer* tail;
+} ErlTimerHead;
 
 /* BEGIN tiw_lock protected variables 
 **
@@ -92,15 +94,33 @@ static erts_smp_mtx_t tiw_lock;
 */
 
 #ifdef SMALL_MEMORY
-#define TIW_SIZE 8192
+#define DEF_TIW_SIZE 8192
 #else
-#define TIW_SIZE 65536		/* timing wheel size (should be a power of 2) */
+#define DEF_TIW_SIZE 65536	/* timing wheel size (should be a power of 2) */
 #endif
-static ErlTimer** tiw;		/* the timing wheel, allocated in init_time() */
-static Uint tiw_pos;		/* current position in wheel */
-static Uint tiw_nto;		/* number of timeouts in wheel */
-static Uint tiw_min;
-static ErlTimer *tiw_min_ptr;
+static Uint tiw_size;
+#define TIW_SIZE tiw_size
+#define MIN_TIW_SIZE 512
+
+#ifdef ERTS_SMP
+#define DEF_TIW_INSTANCES erts_no_schedulers
+#else
+#define DEF_TIW_INSTANCES 1
+#endif
+#define MIN_TIW_INSTANCES 1
+#define MAX_TIW_INSTANCES 128
+
+typedef struct ErlTimerWheel_ {
+    erts_smp_mtx_t tiw_lock;
+    ErlTimerHead* tiw;
+    Uint tiw_pos;
+    Uint tiw_nto;
+    erts_smp_atomic32_t tiw_min;
+    ErlTimer* tiw_min_ptr;
+} ErlTimerWheel;
+
+static ErlTimerWheel* tiwtab;
+static Uint no_tiw;
 
 /* END tiw_lock protected variables */
 
@@ -130,67 +150,73 @@ static ERTS_INLINE void do_time_init(void)
     erts_smp_atomic32_init_nob(&do_time, 0);
 }
 
+static ERTS_INLINE erts_aint32_t read_tiw_min (ErlTimerWheel* tiw)
+{
+    return erts_smp_atomic32_read_acqb(&tiw->tiw_min);
+}
+
+static ERTS_INLINE void set_tiw_min (ErlTimerWheel* tiw, erts_aint32_t val)
+{
+    erts_smp_atomic32_set_acqb(&tiw->tiw_min, val);
+}
+
+static ERTS_INLINE void reset_tiw_min (ErlTimerWheel* tiw)
+{
+    set_tiw_min(tiw, -1);
+}
+
 /* get the time (in units of TIW_ITIME) to the next timeout,
    or -1 if there are no timeouts                     */
 
-static erts_short_time_t next_time_internal(void) /* PRE: tiw_lock taken by caller */
+static erts_short_time_t next_time_internal(ErlTimerWheel* tiw) /* PRE: tiw_lock taken by caller */
 {
     int i, tm, nto;
     Uint32 min;
     ErlTimer* p;
-    erts_short_time_t dt;
   
-    if (tiw_nto == 0)
+    if (tiw->tiw_nto == 0)
 	return -1;	/* no timeouts in wheel */
 
-    if (tiw_min_ptr) {
-	min = tiw_min;
-	dt  = do_time_read();
-	return ((min >= dt) ? (min - dt) : 0);
+    if (tiw->tiw_min_ptr) {
+	return read_tiw_min(tiw);
     }
   
     /* start going through wheel to find next timeout */
     tm = nto = 0;
     min = (Uint32) -1;	/* max Uint32 */
-    i = tiw_pos;
+    i = tiw->tiw_pos;
     do {
-	p = tiw[i];
+	p = tiw->tiw[i].head;
 	while (p != NULL) {
 	    nto++;
 	    if (p->count == 0) {
 		/* found next timeout */
-		dt = do_time_read();
 		/* p->count is zero */
-		tiw_min_ptr = p;
-		tiw_min     = tm;
-		return ((tm >= dt) ? (tm - dt) : 0);
+		tiw->tiw_min_ptr = p;
+		set_tiw_min(tiw, tm);
+		return tm;
 	    } else {
 		/* keep shortest time in 'min' */
 		if (tm + p->count*TIW_SIZE < min) {
 		    min = tm + p->count*TIW_SIZE;
-		    tiw_min_ptr = p;
-		    tiw_min     = min;
+		    tiw->tiw_min_ptr = p;
+		    set_tiw_min(tiw, min);
 		}
 	    }
 	    p = p->next;
 	}
 	/* when we have found all timeouts the shortest time will be in min */
-	if (nto == tiw_nto) break;
+	if (nto == tiw->tiw_nto) break;
 	tm++;
 	i = (i + 1) % TIW_SIZE;
-    } while (i != tiw_pos);
-    dt = do_time_read();
-    if (min <= (Uint32) dt)
-	return 0;
-    if ((min - (Uint32) dt) > (Uint32) ERTS_SHORT_TIME_T_MAX)
-	return ERTS_SHORT_TIME_T_MAX;
-    return (erts_short_time_t) (min - (Uint32) dt);
+    } while (i != tiw->tiw_pos);
+    return min;
 }
 
-static void remove_timer(ErlTimer *p) {
+static void remove_timer(ErlTimerWheel* tiw, ErlTimer *p) {
     /* first */
     if (!p->prev) {
-	tiw[p->slot] = p->next;
+	tiw->tiw[p->slot].head = p->next;
 	if(p->next)
 	    p->next->prev = NULL;
     } else {
@@ -199,6 +225,7 @@ static void remove_timer(ErlTimer *p) {
 
     /* last */
     if (!p->next) {
+	tiw->tiw[p->slot].tail = p->prev;
 	if (p->prev)
 	    p->prev->next = NULL;
     } else {
@@ -209,22 +236,50 @@ static void remove_timer(ErlTimer *p) {
     p->prev = NULL;
     /* Make sure cancel callback isn't called */
     p->active = 0;
-    tiw_nto--;
+    tiw->tiw_nto--;
 }
 
 /* Private export to erl_time_sup.c */
 erts_short_time_t erts_next_time(void)
 {
-    erts_short_time_t ret;
+    erts_short_time_t ret, dt;
+    Uint32 min;
+    int n;
 
-    erts_smp_mtx_lock(&tiw_lock);
-    (void)do_time_update();
-    ret = next_time_internal();
-    erts_smp_mtx_unlock(&tiw_lock);
-    return ret;
+    dt = do_time_update();
+    for (n = 0; n < no_tiw; ++n) {
+	ret = read_tiw_min(tiwtab + n);
+	if (ret >= 0 && ret <= dt) {
+	    return 0;
+	}
+    }
+
+    min = (Uint32) -1; /* max Uint32 */
+    for (n = 0; n < no_tiw; ++n) {
+	ErlTimerWheel* tiw = tiwtab + n;
+	ret = read_tiw_min(tiw);
+	if (ret < 0) {
+	    erts_smp_mtx_lock(&tiw->tiw_lock);
+	    ret = next_time_internal(tiw);
+	    erts_smp_mtx_unlock(&tiw->tiw_lock);
+	}
+	if (ret >= 0) {
+	    dt = do_time_update();
+	    if (ret <= dt) {
+		return 0;
+	    }
+	    if ((ret - (Uint32) dt) > (Uint32) ERTS_SHORT_TIME_T_MAX) {
+		ret = ERTS_SHORT_TIME_T_MAX;
+	    }
+	    if (ret < min) {
+		min = ret;
+	    }
+	}
+    }
+    return min;
 }
 
-static ERTS_INLINE void bump_timer_internal(erts_short_time_t dt) /* PRE: tiw_lock is write-locked */
+static ERTS_INLINE void bump_timer_internal(ErlTimerWheel* tiw, erts_short_time_t dt) /* PRE: tiw_lock is write-locked */
 {
     Uint keep_pos;
     Uint count;
@@ -232,14 +287,14 @@ static ERTS_INLINE void bump_timer_internal(erts_short_time_t dt) /* PRE: tiw_lo
     Uint dtime = (Uint) dt;  
 
     /* no need to bump the position if there aren't any timeouts */
-    if (tiw_nto == 0) {
-	erts_smp_mtx_unlock(&tiw_lock);
+    if (tiw->tiw_nto == 0) {
+	erts_smp_mtx_unlock(&tiw->tiw_lock);
 	return;
     }
 
     /* if do_time > TIW_SIZE we want to go around just once */
     count = (Uint)(dtime / TIW_SIZE) + 1;
-    keep_pos = (tiw_pos + dtime) % TIW_SIZE;
+    keep_pos = (tiw->tiw_pos + dtime) % TIW_SIZE;
     if (dtime > TIW_SIZE) dtime = TIW_SIZE;
   
     timeout_head = NULL;
@@ -247,19 +302,19 @@ static ERTS_INLINE void bump_timer_internal(erts_short_time_t dt) /* PRE: tiw_lo
     while (dtime > 0) {
 	/* this is to decrease the counters with the right amount */
 	/* when dtime >= TIW_SIZE */
-	if (tiw_pos == keep_pos) count--;
-	prev = &tiw[tiw_pos];
+	if (tiw->tiw_pos == keep_pos) count--;
+	prev = &tiw->tiw[tiw->tiw_pos].head;
 	while ((p = *prev) != NULL) {
 	    ASSERT( p != p->next);
 	    if (p->count < count) {     /* we have a timeout */
 		/* remove min time */
-		if (tiw_min_ptr == p) {
-		    tiw_min_ptr = NULL;
-		    tiw_min = 0;
+		if (tiw->tiw_min_ptr == p) {
+		    tiw->tiw_min_ptr = NULL;
+		    reset_tiw_min(tiw);
 		}
 
 		/* Remove from list */
-		remove_timer(p);
+		remove_timer(tiw, p);
 		*timeout_tail = p;	/* Insert in timeout queue */
 		timeout_tail = &p->next;
 	    }
@@ -269,14 +324,15 @@ static ERTS_INLINE void bump_timer_internal(erts_short_time_t dt) /* PRE: tiw_lo
 		prev = &p->next;
 	    }
 	}
-	tiw_pos = (tiw_pos + 1) % TIW_SIZE;
+	tiw->tiw_pos = (tiw->tiw_pos + 1) % TIW_SIZE;
 	dtime--;
     }
-    tiw_pos = keep_pos;
-    if (tiw_min_ptr)
-	tiw_min -= dt;
+    tiw->tiw_pos = keep_pos;
+    if (tiw->tiw_min_ptr) {
+	set_tiw_min(tiw, read_tiw_min(tiw) - dt);
+    }
     
-    erts_smp_mtx_unlock(&tiw_lock);
+    erts_smp_mtx_unlock(&tiw->tiw_lock);
     
     /* Call timedout timers callbacks */
     while (timeout_head) {
@@ -297,14 +353,18 @@ static ERTS_INLINE void bump_timer_internal(erts_short_time_t dt) /* PRE: tiw_lo
 
 void erts_bump_timer(erts_short_time_t dt) /* dt is value from do_time */
 {
-    erts_smp_mtx_lock(&tiw_lock);
-    bump_timer_internal(dt);
+    int i;
+
+    for (i = 0; i < no_tiw; ++i) {
+	erts_smp_mtx_lock(&tiwtab[i].tiw_lock);
+	bump_timer_internal(tiwtab+i, dt);
+    }
 }
 
 Uint
 erts_timer_wheel_memory_size(void)
 {
-    return (Uint) TIW_SIZE * sizeof(ErlTimer*);
+    return (Uint) TIW_SIZE * sizeof(ErlTimer*) * no_tiw;
 }
 
 /* this routine links the time cells into a free list at the start
@@ -313,6 +373,9 @@ void
 erts_init_time(void)
 {
     int i, itime;
+    char buf[21]; /* enough for any 64-bit integer */
+    size_t bufsize;
+    int n;
 
     /* system dependent init; must be done before do_time_init()
        if timer thread is enabled */
@@ -325,16 +388,37 @@ erts_init_time(void)
     tiw_itime = itime;
 #endif
 
-    erts_smp_mtx_init(&tiw_lock, "timer_wheel");
-
-    tiw = (ErlTimer**) erts_alloc(ERTS_ALC_T_TIMER_WHEEL,
-				  TIW_SIZE * sizeof(ErlTimer*));
-    for(i = 0; i < TIW_SIZE; i++)
-	tiw[i] = NULL;
+    bufsize = sizeof(buf);
+    if (erts_sys_getenv("ERL_TIMER_WHEEL_SIZE", buf, &bufsize) == 0) {
+	tiw_size = atoi(buf);
+	if (tiw_size < MIN_TIW_SIZE) {
+	    erl_exit(ERTS_ABORT_EXIT, "invalid ERL_TIMER_WHEEL_SIZE setting: %s", buf);
+	}
+    } else {
+	tiw_size = DEF_TIW_SIZE;
+    }
+    bufsize = sizeof(buf);
+    if (erts_sys_getenv("ERL_TIMER_WHEEL_INSTANCES", buf, &bufsize) == 0) {
+	no_tiw = atoi(buf);
+	if (no_tiw < MIN_TIW_INSTANCES || no_tiw > MAX_TIW_INSTANCES) {
+	    erl_exit(ERTS_ABORT_EXIT, "invalid ERL_TIMER_WHEEL_INSTANCES setting: %s", buf);
+	}
+    } else {
+	no_tiw = DEF_TIW_INSTANCES;
+    }
+    tiwtab = (ErlTimerWheel*)erts_alloc(ERTS_ALC_T_TIMER_WHEEL, no_tiw * sizeof(tiwtab[0]));
+    for (n = 0; n < no_tiw; ++n) {
+	ErlTimerWheel* tiw = tiwtab+n;
+	erts_smp_mtx_init(&tiw->tiw_lock, "timer_wheel");
+	tiw->tiw = (ErlTimerHead*) erts_alloc(ERTS_ALC_T_TIMER_WHEEL,
+				  TIW_SIZE * sizeof(tiw->tiw[0]));
+	for(i = 0; i < TIW_SIZE; i++)
+	    tiw->tiw[i].head = tiw->tiw[i].tail = NULL;
+	tiw->tiw_pos = tiw->tiw_nto = 0;
+	tiw->tiw_min_ptr = NULL;
+	reset_tiw_min(tiw);
+    }
     do_time_init();
-    tiw_pos = tiw_nto = 0;
-    tiw_min_ptr = NULL;
-    tiw_min = 0;
 }
 
 
@@ -344,7 +428,7 @@ erts_init_time(void)
 ** Insert a process into the time queue, with a timeout 't'
 */
 static void
-insert_timer(ErlTimer* p, Uint t)
+insert_timer(ErlTimerWheel* tiw, ErlTimer* p, Uint t)
 {
     Uint tm;
     Uint64 ticks;
@@ -363,49 +447,62 @@ insert_timer(ErlTimer* p, Uint t)
     ticks += do_time_update(); /* Add backlog of unprocessed time */
     
     /* calculate slot */
-    tm = (ticks + tiw_pos) % TIW_SIZE;
+    tm = (ticks + tiw->tiw_pos) % TIW_SIZE;
     p->slot = (Uint) tm;
     p->count = (Uint) (ticks / TIW_SIZE);
   
-    /* insert at head of list at slot */
-    p->next = tiw[tm];
-    p->prev = NULL;
-    if (p->next != NULL)
-	p->next->prev = p;
-    tiw[tm] = p;
-
+    if (tiw->tiw[tm].head == NULL || p->count <= tiw->tiw[tm].head->count) {
+	/* insert at head of list at slot */
+	p->prev = NULL;
+	p->next = tiw->tiw[tm].head;
+	if (p->next) {
+	    p->next->prev = p;
+	} else {
+	    tiw->tiw[tm].tail = p;
+	}
+	tiw->tiw[tm].head = p;
+    } else {
+	/* insert at tail of list at slot */
+	p->next = NULL;
+	p->prev = tiw->tiw[tm].tail;
+	p->prev->next = p;
+	tiw->tiw[tm].tail = p;
+    }
 
     /* insert min time */
-    if ((tiw_nto == 0) || ((tiw_min_ptr != NULL) && (ticks < tiw_min))) {
-	tiw_min = ticks;
-	tiw_min_ptr = p;
+    if ((tiw->tiw_nto == 0) || ((tiw->tiw_min_ptr != NULL) && (ticks < read_tiw_min(tiw)))) {
+	tiw->tiw_min_ptr = p;
+	set_tiw_min(tiw, ticks);
     }
-    if ((tiw_min_ptr == p) && (ticks > tiw_min)) {
+    if ((tiw->tiw_min_ptr == p) && (ticks > read_tiw_min(tiw))) {
 	/* some other timer might be 'min' now */
-	tiw_min = 0;
-	tiw_min_ptr = NULL;
+	tiw->tiw_min_ptr = NULL;
+	reset_tiw_min(tiw);
     }
 
-    tiw_nto++;
+    tiw->tiw_nto++;
 }
 
 void
 erts_set_timer(ErlTimer* p, ErlTimeoutProc timeout, ErlCancelProc cancel,
 	      void* arg, Uint t)
 {
+    int n;
 
     erts_deliver_time();
-    erts_smp_mtx_lock(&tiw_lock);
+    n = erts_get_scheduler_id() % no_tiw;
+    erts_smp_mtx_lock(&tiwtab[n].tiw_lock);
     if (p->active) { /* XXX assert ? */
-	erts_smp_mtx_unlock(&tiw_lock);
+	erts_smp_mtx_unlock(&tiwtab[n].tiw_lock);
 	return;
     }
     p->timeout = timeout;
     p->cancel = cancel;
     p->arg = arg;
     p->active = 1;
-    insert_timer(p, t);
-    erts_smp_mtx_unlock(&tiw_lock);
+    p->instance = n;
+    insert_timer(tiwtab+n, p, t);
+    erts_smp_mtx_unlock(&tiwtab[n].tiw_lock);
 #if defined(ERTS_SMP)
     if (t <= (Uint) ERTS_SHORT_TIME_T_MAX)
 	erts_sys_schedule_interrupt_timed(1, (erts_short_time_t) t);
@@ -415,27 +512,27 @@ erts_set_timer(ErlTimer* p, ErlTimeoutProc timeout, ErlCancelProc cancel,
 void
 erts_cancel_timer(ErlTimer* p)
 {
-    erts_smp_mtx_lock(&tiw_lock);
+    ErlTimerWheel* tiw = tiwtab + p->instance;
+
+    erts_smp_mtx_lock(&tiw->tiw_lock);
     if (!p->active) { /* allow repeated cancel (drivers) */
-	erts_smp_mtx_unlock(&tiw_lock);
+	erts_smp_mtx_unlock(&tiw->tiw_lock);
 	return;
     }
 
     /* is it the 'min' timer, remove min */
-    if (p == tiw_min_ptr) {
-	tiw_min_ptr = NULL;
-	tiw_min     = 0;
+    if (p == tiw->tiw_min_ptr) {
+	tiw->tiw_min_ptr = NULL;
+	reset_tiw_min(tiw);
     }
 
-    remove_timer(p);
+    remove_timer(tiw, p);
     p->slot = p->count = 0;
 
+    erts_smp_mtx_unlock(&tiw->tiw_lock);
     if (p->cancel != NULL) {
-	erts_smp_mtx_unlock(&tiw_lock);
 	(*p->cancel)(p->arg);
-	return;
     }
-    erts_smp_mtx_unlock(&tiw_lock);
 }
 
 /*
@@ -449,57 +546,71 @@ erts_time_left(ErlTimer *p)
 {
     Uint left;
     erts_short_time_t dt;
+    ErlTimerWheel* tiw;
 
-    erts_smp_mtx_lock(&tiw_lock);
+    tiw = tiwtab + p->instance;
+    erts_smp_mtx_lock(&tiw->tiw_lock);
 
     if (!p->active) {
-	erts_smp_mtx_unlock(&tiw_lock);
+	erts_smp_mtx_unlock(&tiw->tiw_lock);
 	return 0;
     }
 
-    if (p->slot < tiw_pos)
-	left = (p->count + 1) * TIW_SIZE + p->slot - tiw_pos;
+    if (p->slot < tiw->tiw_pos)
+	left = (p->count + 1) * TIW_SIZE + p->slot - tiw->tiw_pos;
     else
-	left = p->count * TIW_SIZE + p->slot - tiw_pos;
+	left = p->count * TIW_SIZE + p->slot - tiw->tiw_pos;
     dt = do_time_read();
     if (left < dt)
 	left = 0;
     else
 	left -= dt;
 
-    erts_smp_mtx_unlock(&tiw_lock);
+    erts_smp_mtx_unlock(&tiw->tiw_lock);
 
     return (Uint) left * TIW_ITIME;
 }
 
 #ifdef DEBUG
-void erts_p_slpq(void)
+
+static void
+p_slqq_internal (ErlTimerWheel* tiw)
 {
     int i;
     ErlTimer* p;
   
-    erts_smp_mtx_lock(&tiw_lock);
+    erts_smp_mtx_lock(&tiw->tiw_lock);
 
     /* print the whole wheel, starting at the current position */
-    erts_printf("\ntiw_pos = %d tiw_nto %d\n", tiw_pos, tiw_nto);
-    i = tiw_pos;
-    if (tiw[i] != NULL) {
+    erts_printf("\ntiw_pos = %d tiw_nto %d\n", tiw->tiw_pos, tiw->tiw_nto);
+    i = tiw->tiw_pos;
+    if (tiw->tiw[i].head != NULL) {
 	erts_printf("%d:\n", i);
-	for(p = tiw[i]; p != NULL; p = p->next) {
+	for(p = tiw->tiw[i].head; p != NULL; p = p->next) {
 	    erts_printf(" (count %d, slot %d)\n",
 			p->count, p->slot);
 	}
     }
-    for(i = (i+1)%TIW_SIZE; i != tiw_pos; i = (i+1)%TIW_SIZE) {
-	if (tiw[i] != NULL) {
+    for(i = (i+1)%TIW_SIZE; i != tiw->tiw_pos; i = (i+1)%TIW_SIZE) {
+	if (tiw->tiw[i].head != NULL) {
 	    erts_printf("%d:\n", i);
-	    for(p = tiw[i]; p != NULL; p = p->next) {
+	    for(p = tiw->tiw[i].head; p != NULL; p = p->next) {
 		erts_printf(" (count %d, slot %d)\n",
 			    p->count, p->slot);
 	    }
 	}
     }
 
-    erts_smp_mtx_unlock(&tiw_lock);
+    erts_smp_mtx_unlock(&tiw->tiw_lock);
 }
+
+void erts_p_slpq(void)
+{
+    int n;
+
+    for (n = 0; n < no_tiw; ++n) {
+	p_slpq_internal(tiwtab + n);
+    }
+}
+
 #endif /* DEBUG */
